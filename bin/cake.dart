cd111fcfdececac2d9b9e2142e3c1d79d9e7b145
CLeeSpruit <c.lee.spruit@polyhedra.studio>
2022-07-16 13:31:47 -0500

    Moved runner to its own file

--- bin/cake.dart
+++ bin/cake.dart
@@ -4,23 +4,14 @@ import 'package:cake/cake.dart';
 import 'package:cake/helper/printer.dart';
 
 import 'collector.dart';
+import 'runner.dart';
 import 'settings.dart';
 
 void main(List<String> arguments) async {
   CakeSettings settings = CakeSettings(arguments);
+  Runner runner = Runner();
 
-  // Search for all files in this directory that end with _cake or .cake in them
-  Stream<FileSystemEntity> streamList = Directory.current.list(recursive: true);
-  Stream<FileSystemEntity> cakeStreamList = streamList.where((event) {
-    String filename = event.path.split('/').last;
-    bool cakeFile = filename.endsWith('.cake.dart');
-    if (cakeFile && settings.fileFilter != null) {
-      return filename.contains(settings.fileFilter!);
-    }
-    return cakeFile;
-  });
-
-  List<FileSystemEntity> cakeList = await cakeStreamList.toList();
+  List<FileSystemEntity> cakeList = await runner.cakeFileList;
   if (cakeList.isEmpty) {
     return TestMessage('Cake Test Runner',
             message:
@@ -28,34 +19,14 @@ void main(List<String> arguments) async {
         .report();
   }
 
-  Collector collector = Collector();
+  if (!settings.interactive) {
+    Collector collector = await runner.run(settings);
 
-  // Run all the runners
-  Iterable<Future<void>> processes =
-      cakeList.map<Future<void>>((cakeTest) async {
-    // If we're filtering by a keyword, this needs to be passed via define
-    List<String> processArgs = settings.testFilter.toProperties();
-    processArgs.add(cakeTest.path);
+    // Print results
+    collector.printMessage(settings.verbose);
+    Printer.neutral(''); // This makes sure to clear out any color changes
+    return;
+  }
 
-    return Process.run('dart', processArgs).then((ProcessResult result) {
-      if (result.stderr is String && result.stderr.isNotEmpty) {
-        print(result.stderr);
-      }
-      List<TestRunnerCollector> testRunnerCollectors =
-          testRunnerOutputParser(result.stdout);
-      for (TestRunnerCollector element in testRunnerCollectors) {
-        collector.addCollector(element);
-      }
-
-      if (settings.verbose) {
-        print('${collector.total} tests found...');
-      }
-    });
-  });
-
-  await Future.wait(processes);
-
-  // Print results
-  collector.printMessage(settings.verbose);
-  Printer.neutral(''); // This makes sure to clear out any color changes
+  // Interactive mode
 }
